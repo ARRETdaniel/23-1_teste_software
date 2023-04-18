a5895e2878c1f03838b2885ce1de1d7caddef160
Daniel Terra Gomes <danielterra24@hotmail.com>
2023-04-18 17:24:02 -0300

    new tests added & updated

--- lista_teste_unidade/lista_de_exercicios/utilidades/utils.py
+++ lista_teste_unidade/lista_de_exercicios/utilidades/utils.py
@@ -60,6 +60,8 @@ def sort_data(data):
 
 # cars
 
+# 7
+
 
 def car_consumption():
     data = []
@@ -73,3 +75,122 @@ def car_consumption():
         consumption_per_kilometer = consumption*1000
         data.append((name, consumption, consumption_per_kilometer))
     return sorted(data, key=lambda x: x[1])
+# 8
+
+
+def read_string():
+    s1 = input("Digite a string S1 (com no máximo 20 caracteres): ")
+    if len(s1) > 20:
+        print("A string digitada possui mais de 20 caracteres. Serão considerados apenas os primeiros 20 caracteres.")
+        s1 = s1[:20]
+    return s1
+
+
+def print_string_length(s):
+    print("O tamanho da string é:", len(s))
+
+
+def compare_strings(s1, s2):
+    if s1 == s2:
+        print("As strings são iguais.")
+    else:
+        print("As strings são diferentes.")
+
+
+def concatenate_strings(s1, s2):
+    print("A concatenação das strings é:", s1 + s2)
+
+
+def reverse_string(s):
+    print("A string invertida é:", s[::-1])
+
+
+def count_character(s, c):
+    print("A quantidade de vezes que o caractere aparece na string é:", s.count(c))
+
+
+def replace_character(s, c1, c2):
+    s = s.replace(c1, c2, 1)
+    print("A string após a substituição é:", s)
+
+
+def is_substring(s1, s2):
+    if s2 in s1:
+        print("A string", s2, "é uma substring de", s1)
+    else:
+        print("A string", s2, "não é uma substring de", s1)
+
+
+def get_substring(s, start, length):
+    substring = s[start:start+length]
+    print("A substring é:", substring)
+
+
+def show_menu():
+    print("Escolha uma opção:")
+    print("(a) Ler uma string S1")
+    print("(b) Imprimir o tamanho da string S1")
+    print("(c) Comparar a string S1 com uma nova string S2")
+    print("(d) Concatenar a string S1 com uma nova string S2")
+    print("(e) Imprimir a string S1 de forma reversa")
+    print("(f) Contar quantas vezes um dado caractere aparece na string S1")
+    print("(g) Substituir a primeira ocorrencia do caractere C1 da string S1 pelo caractere C2")
+    print("(h) Verificar se uma string S2 é substring de S1")
+    print("(i) Retornar uma substring da string S1")
+    print("(q) Sair")
+
+# 9
+
+
+class Contact:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+
+
+class Phonebook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_contact(self, name, phone_number):
+        contact = Contact(name, phone_number)
+        self.contacts.append(contact)
+
+    def search_contact(self, name):
+        for contact in self.contacts:
+            if contact.name == name:
+                return contact
+        return None
+
+    def remove_contact(self, name):
+        contact = self.search_contact(name)
+        if contact:
+            self.contacts.remove(contact)
+            return True
+        return False
+
+# 10
+
+
+class Person:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+
+class Loan:
+    def __init__(self, person, book, date_borrowed):
+        self.person = person
+        self.book = book
+        self.date_borrowed = date_borrowed
+        self.date_returned = None
+
+    def return_book(self, date_returned):
+        self.date_returned = date_returned
--- lista_teste_unidade/testes/test_main.py
+++ lista_teste_unidade/testes/test_main.py
@@ -1,8 +1,31 @@
 # https://stackoverflow.com/questions/36456920/is-there-a-way-to-specify-which-pytest-tests-to-run-from-a-file
 # pytest -v  test_main.py::test_read_and_print_string
+from io import StringIO
+import sys
+import io
+from unittest.mock import patch
+
 import pytest
 
 from lista_de_exercicios.utilidades.utils import *
+
+# 1
+
+
+def test_read_and_print_string(monkeypatch, capsys):
+    # Prepare test input
+    input_str = "Hello, world!"
+    monkeypatch.setattr('builtins.input', lambda _: input_str)
+
+    # Call the function
+    result = read_and_print_string()
+
+    # Check the output and return value
+    assert result == input_str
+    captured = capsys.readouterr()
+    assert captured.out.strip() == input_str
+
+# 2
 
 
 def test_get_string_length():
@@ -11,35 +34,18 @@ def test_get_string_length():
     assert get_string_length("こんにちは世界") == 7
     assert get_string_length("Python is awesome") == 17
 
-'''
-# The capsys fixture provided by Pytest allows us to capture stdout output during the execution of our test function. We use this fixture to capture the output of our read_and_print_string function.
-def test_read_and_print_string(capsys):
-    input_str = "Hello, world!"
-    expected_output = "Hello, world!"
-
-    # Use the `capsys` fixture to capture stdout output
-    # during function execution
-    captured_output = capsys.readouterr()
-
-    # Call the function
-    result = read_and_print_string(input_str)
-
-    # Check that the function returns the input string
-    assert result == expected_output
-
-    # Check that the function prints the input string
-    assert captured_output.out == input_str + "\n"
-'''
-# test_count_ones.py
+# 3
 
 
-def test_count_ones():
+def test_count_ones_in_a_string():
     assert count_ones("") == 0
     assert count_ones("hello world") == 0
     assert count_ones("1") == 1
     assert count_ones("111001011") == 6
 
-'''
+# 4
+
+
 def test_print_name_if_starts_with_a(capsys):
     # Case 1: Name starts with 'a' in lowercase
     print_name_if_starts_with_a('alice')
@@ -55,36 +61,125 @@ def test_print_name_if_starts_with_a(capsys):
     print_name_if_starts_with_a('bob')
     captured = capsys.readouterr()
     assert captured.out == ''
-'''
 
-def test_remove_spaces():
+# 5
+
+
+def test_remove_spaces_of_a_vector_and_return_it():
     assert remove_spaces('hello world') == [
         'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
     assert remove_spaces('the quick brown fox') == [
         't', 'h', 'e', 'q', 'u', 'i', 'c', 'k', 'b', 'r', 'o', 'w', 'n', 'f', 'o', 'x']
 
-'''
-# undone
-def test_read_input(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: 'Alice')
-    monkeypatch.setattr('builtins.input', lambda _: '25')
-    monkeypatch.setattr('builtins.input', lambda _: 'Bob')
-    monkeypatch.setattr('builtins.input', lambda _: '35')
-    monkeypatch.setattr('builtins.input', lambda _: 'quit')
-    assert read_input() == [('Alice', 25), ('Bob', 35)]
+
+# test_read_input.py
+# 6
+def test_read_input_and_when_a_negative_value_is_entered_the_program_must_stop_and_return_the_ages_sorted(monkeypatch):
+    # Prepare input values
+    input_values = iter(['John', '25', 'Mary', '30', 'quit'])
+
+    def mock_input(prompt=None):
+        return next(input_values)
+
+    # Monkeypatch input() function
+    monkeypatch.setattr('builtins.input', mock_input)
+
+    # Call the function and verify the output
+    expected_output = [('John', 25), ('Mary', 30)]
+    assert read_input() == expected_output
+
+# 6
 
 
 def test_sort_data():
-    data = [('Alice', 25), ('Bob', 35), ('Charlie', 30)]
-    assert sort_data(data) == [('Alice', 25), ('Charlie', 30), ('Bob', 35)]
+    data = [("John", 28), ("Mary", 22), ("Bob", 31), ("Alice", 24)]
+    expected_result = [("Mary", 22), ("Alice", 24), ("John", 28), ("Bob", 31)]
+    assert sort_data(data) == expected_result
 
-#undone
+# 7
+
+
 def test_car_consumption(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: 'ausca')
-    monkeypatch.setattr('builtins.input', lambda _: '1')
-    monkeypatch.setattr('builtins.input', lambda _: 'Bob')
-    monkeypatch.setattr('builtins.input', lambda _: '35')
-    monkeypatch.setattr('builtins.input', lambda _: 'quit')
-    assert car_consumption() == []
-    #assert car_consumption() == [('ausca', 1.0, 1000.0), ('Bob', 35.0, 35000.0)]
-'''
+    # Test case 1: Normal case
+    user_input = ['Toyota', '10', 'quit']
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda _: user_input.pop(0))
+        result = car_consumption()
+    expected_result = [('Toyota', 10.0, 10000.0)]
+    assert result == expected_result
+
+    # Test case 2: Invalid consumption input
+    user_input = ['Toyota', '-10', 'quit']
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda _: user_input.pop(0))
+        result = car_consumption()
+    expected_result = []
+    assert result == expected_result
+
+    # Test case 3: Quit input before entering data
+    user_input = ['quit']
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda _: user_input.pop(0))
+        result = car_consumption()
+    expected_result = []
+    assert result == expected_result
+# 8
+
+# 9
+
+
+def test_add_contact():
+    phonebook = Phonebook()
+    phonebook.add_contact("John Doe", "123-456-7890")
+    assert phonebook.search_contact("John Doe").phone_number == "123-456-7890"
+
+
+def test_search_contact():
+    phonebook = Phonebook()
+    phonebook.add_contact("John Doe", "123-456-7890")
+    phonebook.add_contact("Jane Doe", "234-567-8901")
+    assert phonebook.search_contact("John Doe").phone_number == "123-456-7890"
+    assert phonebook.search_contact("Jane Doe").phone_number == "234-567-8901"
+    assert phonebook.search_contact("Bob Smith") is None
+
+
+def test_remove_contact():
+    phonebook = Phonebook()
+    phonebook.add_contact("John Doe", "123-456-7890")
+    phonebook.add_contact("Jane Doe", "234-567-8901")
+    assert phonebook.remove_contact("John Doe") is True
+    assert phonebook.search_contact("John Doe") is None
+    assert phonebook.remove_contact("Bob Smith") is False
+
+# 10
+
+
+def test_loan():
+    person = Person("John Doe", "johndoe@example.com")
+    book = Book("The Catcher in the Rye", "J.D. Salinger", "9780316769488")
+    loan = Loan(person, book, "2022-01-01")
+    assert loan.person == person
+    assert loan.book == book
+    assert loan.date_borrowed == "2022-01-01"
+    assert loan.date_returned == None
+
+
+def test_return_book():
+    person = Person("John Doe", "johndoe@example.com")
+    book = Book("The Catcher in the Rye", "J.D. Salinger", "9780316769488")
+    loan = Loan(person, book, "2022-01-01")
+    loan.return_book("2022-01-10")
+    assert loan.date_returned == "2022-01-10"
+
+
+def test_person():
+    person = Person("John Doe", "johndoe@example.com")
+    assert person.name == "John Doe"
+    assert person.email == "johndoe@example.com"
+
+
+def test_book():
+    book = Book("The Catcher in the Rye", "J.D. Salinger", "9780316769488")
+    assert book.title == "The Catcher in the Rye"
+    assert book.author == "J.D. Salinger"
+    assert book.isbn == "9780316769488"
